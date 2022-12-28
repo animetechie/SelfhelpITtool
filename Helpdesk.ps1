d00b92a001feecf996d4f80f833ceaf7e95232f9
Ryan Wright <117471213+animetechie@users.noreply.github.com>
2022-12-28 16:03:07 -0600

    Update Helpdesk.ps1

--- Helpdesk.ps1
+++ Helpdesk.ps1
@@ -14,8 +14,8 @@ $from = "user@example.com"
 $to = "helpdesk@example.com"
 
 # Set the subject and body of the email
-$subject = "Hardware Issue"
-$body = "I am experiencing a hardware issue and would like assistance. Please contact me as soon as possible."
+$subject = "Hardware/Software Issue"
+$body = "I am experiencing a hardware/software issue and would like assistance. Please contact me as soon as possible."
 
 # Prompt the user for input
 $response = Read-Host "Are you having hardware or software issues? (Enter 'hardware' or 'software')"
@@ -55,8 +55,59 @@ if ($response -eq "hardware") {
               Write-Host "Audio driver update complete."
             }
             "no" {
-              # If the response is "no", print
-# If the response is "3" or "4", prompt the user for additional input
+              # If the response is "no", print a message
+              Write-Host "No action taken."
+            }
+            default { Write-Host "Invalid response. Please enter 'yes' or 'no'." }
+          }
+        }
+        "video" {
+          # If the response is "video", prompt the user for additional input
+          $uninstallVideoDriver = Read-Host "Would you like to uninstall and reinstall your video driver? (Enter 'yes' or 'no')"
+
+          # Check the value of the
+"3" {
+  # If the response is "3", check for updates for the printer driver and firmware
+  Write-Host "Checking for updates for printer driver and firmware..."
+
+  # Get a list of installed printers
+  $printers = Get-WmiObject -Class Win32_Printer | Where-Object { $_.Status -eq "OK" }
+
+  # Iterate through the list of printers and update the drivers and firmware for each one
+  foreach ($printer in $printers) {
+    Update-Driver -DeviceId $printer.DeviceID -DriverUpdateOption UpdateAll
+  }
+
+  # Print a message to indicate that the update is complete
+  Write-Host "Printer update complete."
+}
+"4" {
+  # If the response is "4", check for updates for the keyboard and mouse drivers
+  Write-Host "Checking for updates for keyboard and mouse drivers..."
+
+  # Get a list of installed keyboards
+  $keyboards = Get-WmiObject -Class Win32_Keyboard | Where-Object { $_.Status -eq "OK" }
+
+  # Iterate through the list of keyboards and update the drivers for each one
+  foreach ($keyboard in $keyboards) {
+    Update-Driver -DeviceId $keyboard.DeviceID -DriverUpdateOption UpdateAll
+  }
+
+  # Get a list of installed mice
+  $mice = Get-WmiObject -Class Win32_PointingDevice | Where-Object { $_.Status -eq "OK" }
+
+  # Iterate through the list of mice and update the drivers for each one
+  foreach ($mouse in $mice) {
+    Update-Driver -DeviceId $mouse.DeviceID -DriverUpdateOption UpdateAll
+  }
+
+  # Print a message to indicate that the update is complete
+  Write-Host "Keyboard and mouse update complete."
+}
+default { Write-Host "Invalid response. Please enter '1', '2', '3', or '4'." }
+}
+
+# Prompt the user for input
 $sendEmail = Read-Host "Would you like to email the helpdesk? (Enter 'yes' or 'no')"
 
 # Check the value of the sendEmail variable
@@ -73,30 +124,66 @@ switch ($sendEmail) {
   default { Write-Host "Invalid response. Please enter 'yes' or 'no'." }
 }
 }
-"2" {
-  # If the response is "2", check the network adapters
-  Write-Host "Checking network adapters..."
+elseif ($response -eq "software") {
+  # If the response is "software", prompt the user for additional input
+  $softwareIssue = Read-Host "What type of software issue are you experiencing? (Enter '1' for system updates, '2' for application updates, or '3' for software resets)"
 
-  # Get a list of installed network adapters
-  $networkAdapters = Get-WmiObject -Class Win32_NetworkAdapter | Where-Object { $_.Status -eq "OK" }
+  # Check the value of the softwareIssue variable
+  switch ($softwareIssue) {
+   "1" {
+      # If the response is "1", check for system updates
+      Write-Host "Checking for system updates..."
 
-  # Iterate through the list of network adapters and update the drivers for each one
-  foreach ($adapter in $networkAdapters) {
-    Update-Driver -DeviceId $adapter.DeviceID -DriverUpdateOption UpdateAll
+      # Check for system updates
+      Invoke-Expression -Command "wuauclt /detectnow"
+
+      # Print a message to indicate that the update is complete
+      Write-Host "System update complete."
+    }
+    "2" {
+      # If the response is "2", check for application updates
+      Write-Host "Checking for application updates..."
+
+      # Get a list of installed applications
+      $applications = Get-WmiObject -Class Win32_Product | Where-Object { $_.Status -eq "OK" }
+
+      # Iterate through the list of applications and update each one
+      foreach ($application in $applications) {
+        Update-Driver -DeviceId $application.DeviceID -DriverUpdateOption UpdateAll
+      }
+
+      # Print a message to indicate that the update is complete
+      Write-Host "Application update complete."
+    }
+    "3" {
+      # If the response is "3", reset the software
+      Write-Host "Resetting software..."
+
+      # Reset the software
+      Invoke-Expression -Command "sfc /scannow"
+
+      # Print a message to indicate that the reset is complete
+      Write-Host "Software reset complete."
+    }
+    default { Write-Host "Invalid response. Please enter '1', '2', or '3'." }
   }
 
-  # Run the ipconfig /refresh and ipconfig /renew commands
-  Invoke-Expression -Command "ipconfig /refresh"
-  Invoke-Expression -Command "ipconfig /renew"
+  # Prompt the user for input
+  $sendEmail = Read-Host "Would you like to email the helpdesk? (Enter 'yes' or 'no')"
 
-  # Print a message to indicate that the update is complete
-  Write-Host "Network adapter update complete."
+  # Check the value of the sendEmail variable
+  switch ($sendEmail) {
+    "yes" {
+      # If the response is "yes", open the default mail client and send the email
+      Start-Process "mailto:$to?subject=$subject&body=$body"
+      Send-MailMessage -SmtpServer $smtpServer -Port $smtpPort -Credential $credential -From $from -To $to -Subject $subject -Body $body
+    }
+    "no" {
+      # If the response is "no", print a message
+      Write-Host "Thank you. The program will now close."
+    }
+    default { Write-Host "Invalid response. Please enter 'yes' or 'no'." }
+  }
 }
-elseif ($response -eq "software") {
-  # If the response is "software", print a message
-  Write-Host "Please check the documentation or contact the IT support team for assistance with software issues."
-}
-else {
-  # If the response is anything else, print an error message
-  Write-Host "Invalid response. Please enter 'hardware' or 'software'."
-}
+else { Write-Host "Invalid response. Please enter 'hardware' or 'software'." }
+
